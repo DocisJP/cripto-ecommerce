4a37dd0e60e6ef7616ec5fb6954505f66ea13c0d
Pablo <godoypablojuan@gmail.com>
2023-11-14 22:05:13 -0300

    First branch commit

---
+++ src/test/java/com/cripto/ecommerce/app/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cripto.ecommerce.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
